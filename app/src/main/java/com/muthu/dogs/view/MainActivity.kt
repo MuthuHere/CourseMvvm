5e3a1d865d198f4146bebf8968ca513070cb7697
Muthu <s.muthukhumar@gmail.com>
2021-02-08 14:04:24 +0530

    1. libs added 2. navigation graph added with 2 fragments

--- app/src/main/java/com/muthu/dogs/view/MainActivity.kt
+++ app/src/main/java/com/muthu/dogs/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.muthu.dogs
+package com.muthu.dogs.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.muthu.dogs.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
